46a5ca6099783dea43cb9e13f633ea7ebb241a8f
Ilya Khaprov <dead.trickster@gmail.com>
2013-12-14 05:18:45 -0800

    fix todo editing using code from todomvc.com

--- app/components/edit_todo.js
+++ app/components/edit_todo.js
@@ -1,6 +1,8 @@
 // views/edit_todo.js
-export default Ember.Component.extend({
-  didInsertElement: function() {
-    this.$().focus();
-  }
+export default Ember.TextField.extend({
+    focusOnInsert: function () {
+        // Re-set input value to get rid of a reduntant text selection
+        this.$().val(this.$().val());
+        this.$().focus();
+    }.on('didInsertElement')
 });
--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,33 +1,48 @@
 // controllers/todo.js
 export default Ember.ObjectController.extend({
-  isCompleted: function(key, value){
-    var model = this.get('model');
-
-    if (arguments.length === 2) {
-      // property being used as a setter
-      model.set('isCompleted', value);
-      model.save();
-      return value;
-   } else {
-      // property being used as a getter
-      return model.get('isCompleted');
-    }
-  }.property('model.isCompleted'),
-
   isEditing: false,
+
+  // We use the bufferedTitle to store the original value of
+  // the model's title so that we can roll it back later in the
+  // `cancelEditing` action.
+  bufferedTitle: Ember.computed.oneWay('title'),
 
   actions: {
     editTodo: function () {
       this.set('isEditing', true);
     },
+
+    doneEditing: function () {
+      var bufferedTitle = this.get('bufferedTitle').trim();
+
+      if (Ember.isEmpty(bufferedTitle)) {
+        // The `doneEditing` action gets sent twice when the user hits
+        // enter (once via 'insert-newline' and once via 'focus-out').
+        //
+        // We debounce our call to 'removeTodo' so that it only gets
+        // sent once.
+        Ember.run.debounce(this, this.send, 'removeTodo', 0);
+      } else {
+        var todo = this.get('model');
+        todo.set('title', bufferedTitle);
+        todo.save();
+      }
+
+      // Re-set our newly edited title to persist it's trimmed version
+      this.set('bufferedTitle', bufferedTitle);
+      this.set('isEditing', false);
+    },
+
+    cancelEditing: function () {
+      this.set('bufferedTitle', this.get('title'));
+      this.set('isEditing', false);
+    },
+
     removeTodo: function () {
       var todo = this.get('model');
+
       todo.deleteRecord();
       todo.save();
-    },
-    acceptChanges: function () {
-      this.set('isEditing', false);
-      this.get('model').save();
     }
   }
 });
